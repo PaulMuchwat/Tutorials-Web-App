47d495601519d31a9a741d333cc8f74405779d4f
PaulMuchwat <paulmuchwat@gmail.com>
2023-02-08 16:53:29 +0300

    featured section

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_academy/res/assets.dart';
 import 'package:flutter_academy/widgets/course_card.dart';
+import 'package:flutter_academy/widgets/featured_section.dart';
 import 'package:flutter_academy/widgets/header.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,7 +20,7 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 40.0),
           Padding(
             padding: const EdgeInsets.only(left: 20.0),
-            child: Text("Recent Courses",
+            child: Text("Recent Videos",
                 style: Theme.of(context).textTheme.displaySmall),
           ),
           const SizedBox(height: 10.0),
@@ -46,6 +47,27 @@ class HomePage extends StatelessWidget {
                 ),
                 // ... you can add more courses
               ],
+            ),
+          ),
+          Center(
+            child: FeaturedSection(
+              image: Assets.instructor,
+              title: "Start teaching today",
+              description:
+                  "Instructors from around the world teach millions of students on Udemy. We provide the tools and skills to teach what you love.",
+              buttonLabel: "Become an instructor",
+              onActionPressed: () {},
+            ),
+          ),
+          Center(
+            child: FeaturedSection(
+              // imageLeft: false,
+              image: Assets.instructor,
+              title: "Transform your life through education",
+              description:
+                  "Education changes your life beyond your imagination. Education enables you to achieve your dreams.",
+              buttonLabel: "Start learning",
+              onActionPressed: () {},
             ),
           ),
         ],
