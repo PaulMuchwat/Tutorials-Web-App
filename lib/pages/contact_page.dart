e2d2efbdac7517d7d3316d59d07ec8c8554e6458
Paul <paulmuchwat@gmail.com>
2023-02-26 21:45:58 +0300

    contact page

---
+++ lib/pages/contact_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_academy/res/responsive.dart';
+import 'package:flutter_academy/widgets/drawer_nav.dart';
+import 'package:flutter_academy/widgets/top_nav.dart';
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: const <Widget>[
+          TopNav(),
+        ],
+      ),
+      drawer: MediaQuery.of(context).size.width > ScreenSizes.md
+          ? null
+          : const DrawerNav(),
+    );
+  }
+}
