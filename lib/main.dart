d3eb7d71a4b9263aa4723281695cf4a8129a51ea
Paul <paulmuchwat@gmail.com>
2023-02-28 00:36:26 +0300

    advance named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_academy/pages/about_page.dart';
-import 'package:flutter_academy/pages/contact_page.dart';
-import 'package:flutter_academy/pages/courses_page.dart';
 import 'package:flutter_academy/pages/home_page.dart';
 
 void main() {
@@ -19,11 +17,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      routes: {
-        '/': (_) => const HomePage(),
-        '/about': (_) => const AboutPage(),
-        '/contact': (_) => const ContactPage(),
-        '/courses': (_) => const CoursesPage(),
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (_) {
+            switch (settings.name) {
+              case '/':
+                return HomePage();
+              case '/about':
+                return AboutPage();
+              case '/contact':
+                return ContactPage();
+              case '/courses':
+                return CoursesPage();
+              default:
+                final pathSegments = Uri.parse(settings.name!).pathSegments;
+                print(pathSegments);
+                if (pathSegments.length == 2 && pathSegments[0] == 'courses') {
+                  final courseId = int.parse(pathSegments[1]);
+                  return CourseDetailsPage(courseId: courseId);
+                }
+                return Error404Page();
+            }
+          },
+          settings: settings,
+        );
       },
     );
   }
