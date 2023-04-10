50838a6f1eea2f26791cc38b4398c468fa76005d
Paul <paulmuchwat@gmail.com>
2023-04-10 23:52:04 +0300

    fixes

--- lib/app/view_models/watchlist.vm.dart
+++ lib/app/view_models/watchlist.vm.dart
@@ -1,31 +1,26 @@
-import 'package:flutter_academy/app/view_models/auth.vm.dart';
 import 'package:flutter_academy/infrastructure/res/watchlist.service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'course.vm.dart';
 
 class WatchlistVM extends StateNotifier<List<CourseVM>> {
   final WatchlistService _watchlistService;
-  final String? userId;
 
-  WatchlistVM(this._watchlistService, {this.userId}) : super(const []) {
+  WatchlistVM(this._watchlistService) : super(const []) {
     getWatchlist();
   }
 
-  Future<void> addToWatchlist(String id) async {
-    if (userId == null) return;
-    await _watchlistService.addToWatchlist(id, userId!);
+  Future<void> addToWatchlist(String id, String userId) async {
+    await _watchlistService.addToWatchlist(id, userId);
     await getWatchlist();
   }
 
   Future<void> removeFromWatchlist(String id) async {
-    if (userId == null) return;
-    await _watchlistService.removeFromWatchlist(id, userId!);
+    await _watchlistService.removeFromWatchlist(id);
     state = state.where((course) => course.course.id != id).toList();
   }
 
   Future<void> getWatchlist() async {
-    if (userId == null) return;
-    final watchlist = await _watchlistService.getWatchlist(userId!);
+    final watchlist = await _watchlistService.getWatchlist();
     var courses = [];
     for (final course in watchlist) {
       courses = [CourseVM(course), ...state];
@@ -38,8 +33,5 @@ class WatchlistVM extends StateNotifier<List<CourseVM>> {
   }
 }
 
-final watchlistVM =
-    StateNotifierProvider<WatchlistVM, List<CourseVM>>((ref) => WatchlistVM(
-          WatchlistService.instance,
-          userId: ref.watch(authVM.notifier).user?.id,
-        ));
+final watchlistVM = StateNotifierProvider<WatchlistVM, List<CourseVM>>(
+    (_) => WatchlistVM(WatchlistService.instance));
--- lib/app/widgets/drawer_nav.widget.dart
+++ lib/app/widgets/drawer_nav.widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_academy/main.dart';
 
 class DrawerNav extends StatelessWidget {
   const DrawerNav({Key? key}) : super(key: key);
